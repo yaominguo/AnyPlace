9fb3fff18b7b6be6cf78fbefb5faba80c0539469
yaominguo <missgmy@yahoo.com>
2017-12-22 22:48:50 +0800

    项目初始化

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true,
+        "mocha":true
+    },
+    "extends": "eslint:recommended",
+    // "parser":"babel-eslint",
+    "parserOptions":{
+        "ecmaVersion":6,
+        "sourceTYpe":"script"
+    },
+
+    "rules": {
+        //"off"关掉规则,"warn"允许写但是会警告,"error"不允许写
+        "no-console":["error",{
+            "allow":["warn","error","info"]//允许console.warn等
+        }],
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
